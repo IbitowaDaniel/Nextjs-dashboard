6e646a77e6eb39e3aa04cfe7a95ecfa10dc49f2b
Ibitowa Daniel <ibitowadanielfavour@gmail.com>
2023-11-28 11:30:16 -0800

    added and edited files

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,23 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
+}
+
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -70,7 +87,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
   throw new Error('Failed to Delete Invoice');
-  
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
